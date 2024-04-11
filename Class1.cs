7dbbab90fb39779b58745195e261cae7a9c62a6d
PikeCaptain <idunno6418@gmail.com>
2024-04-11 15:03:51 -0500

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    class Greetings
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Traveler! Welcome!");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, thank you for joining us today!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            Greetings greetings = new Greetings(); 
+            //
+            greetings.Welcome();
+
+            //
+            Console.WriteLine("What is your name squire?");
+            string userName = Console.ReadLine();
+            
+            greetings.Hello("landon")
+        }
+    }
+}
